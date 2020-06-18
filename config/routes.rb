8a4348bd6d63812db3669848aeb14878ca584b65
216d0136 <keukemori0226@gmail.com>
2020-06-18 08:57:06 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -24,13 +24,15 @@ end
 
 #customer
     root "products#top"   #会員topページ
+    get 'products/genre_products/:id' => 'products#genre_products', :action => 'genre_products'
     get "home/about" => "homes#about"  #aboutページ
     get '/customers/:id/withdrow' => 'customers#withdrow', as: 'withdrow_customer' #退会画面
     patch '/customers/:id/withdrow' => 'customers#switch', as: 'withdrow_switch_customer'  #退会処理
     delete 'cart_items/destroy_all' => 'cart_items#destroy_all' #カートを空にする
     post '/orders/confirm' => 'orders#confirm', as: 'orders_confirm'  #購入確認ページ
     get '/orders/thanks' => 'orders#thanks'  #注文完了後のお礼ページ(サンクスページ)
-    
+    get '/products/genre_products/:id' => 'products#genre_products', :action => 'genre_products'
+
     resources :customers, :only => [:show, :edit, :update]
     resources :products, :only => [:show, :index]
     resources :cart_items, :only => [:create, :update, :destroy, :index]
