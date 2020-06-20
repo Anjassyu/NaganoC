790242e8400251a269a3e4036f87e177faec4ba1
vagrant <vagrant@localhost.localdomain>
2020-06-20 03:51:57 +0000

    first commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,38 @@
 class OrdersController < ApplicationController
+
+	def new
+		@order = Order.new
+	end
+
+	def confirm
+		@order = Order.new(order_params)
+		@cart_items = current_cart
+	end
+
+	def create
+	end
+
+	def thanks
+	end
+
+	def index
+	end
+
+	def show
+	end
+	
+	private
+
+	def order_params
+	    params.require(:order).permit(:customer_id, 
+	    															:name, 
+	    															:postcode, 
+	    															:address, 
+	    															:postage, 
+	    															:billing_amount, 
+	    															:payment,
+	    															:order_status
+	    															)
+	end
+
 end
