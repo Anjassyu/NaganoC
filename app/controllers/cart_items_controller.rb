2ac7afcb9ad9515d0752514e978d7220db6dac96
vagrant <vagrant@localhost.localdomain>
2020-06-22 08:55:10 +0000

    add cart_item

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,7 +1,8 @@
 class CartItemsController < ApplicationController
 
    def index
-   	   @customer = current_customer
+   	   @customer = Customer.find(current_customer.id)
+         @total_price = calculate(current_customer)
    end
 
    def create
@@ -19,17 +20,33 @@ class CartItemsController < ApplicationController
 
    def destroy
    	   cart_item = CartItem.find(params[:id])
-   	   cart_item.destroy
-   	   redirect_to cart_items_path
+   	if cart_item.destroy
+   	   flash[:notice] = "カート内の商品を削除しました。"
+         redirect_to cart_items_path
+      else
+         render action: :index
+      end
    end
+
    def destroy_all
-   	   current_customer.cart_items.destroy_all
-   	   redirect_to cart_items_path
+   	if current_customer.cart_items.destroy_all
+         flash[:notice] = "カート内の商品を全て削除しました。"
+         redirect_to cart_items_path
+      else
+         redirect_to root_path
+      end
    end
 
    private
    def cart_item_params
-   	   params.require(:cart_item).permit(:count,:customer_id,:puroduct_id)
+   	   params.require(:cart_item).permit(:count,:customer_id,:product_id)
    end
 
+   def calculate(customer)
+     total_price = 0
+     customer.cart_items.each do |cart_item|
+     total_price += cart_item.count * cart_item.product.price
+   end
+     return (total_price * 1.1).floor
+   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,24 @@
 class ProductsController < ApplicationController
 def top
 	@genres = Genre.all
-	@products = Product.all
+	@products = Product.order(created_at: :desc).limit(4)
 end
-
 
 def index
 	@genre = Genre.all
-	@products = Product.all
+	@products = Product.all.page(params[:page]).per(8)
+ #    @genres = Genre.where(effectiveness: true)
+ # if params[:genre_id]
+ #    @genre = Genre.find(params[:genre_id])
+ #    @products = @genre.products.order(created_at: :desc).where(sales_status: "販売中")
+ # else
+ #    @products = Product.where(sales_status: "販売中")
+ # end
+end
+
+def show
+	@product = Product.find(params[:id])
+	@cart_item = CartItem.new
 end
 
 def genre_products
@@ -17,8 +28,8 @@ end
 
 
 private
-def product_params
-	params.require(:product).permit(:name, :description, :price, :image)
-end
+	def product_params
+		params.require(:product).permit(:name, :description, :price, :image)
+	end
 
 end
