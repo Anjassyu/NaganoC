85c6c04c8e889b9db76f445a043a133b5315820b
wakanak2 <w.koba.0214@gmail.com>
2020-06-22 21:51:18 +0900

    [管理者ログイン修正]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,19 @@
 class Admin::OrdersController < ApplicationController
+
+	def index
+		@orders = Order.all.page(params[:page]).per(10)
+	end
+
+	private
+
+	def order_params
+	    params.require(:order).permit(:customer_id,
+	    							  :name,
+	    							  :postcode,
+	    							  :address,
+	    							  :postage,
+	    							  :billing_amount,
+	    							  :payment,
+	    							  :order_status)
+	end
 end
