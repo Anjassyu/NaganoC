cc1f5c38764a4f7eec2446d622e13d148b77be20
wakanak2 <w.koba.0214@gmail.com>
2020-06-23 19:32:58 +0900

    [注文一覧作成]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,13 @@
 class Admin::OrdersController < ApplicationController
 
 	def index
-		@orders = Order.all.page(params[:page]).per(10)
+		if params[:customer_id]
+	  	 @orders = Order.from_customer(params[:customer_id]).page(params[:page]).per(10)
+
+		elsif params[:created_at]
+
+		  @orders = Order.created_today(params[:creted_at]).page(params[:page]).per(10)
+		end
 	end
 
 	private
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,12 @@ class Order < ApplicationRecord
 
 	enum payment: {クレジットカード:1, 銀行振込:2}
 
+	 scope :from_customer, -> (customer_id){ where( customer_id: customer_id )}
+	 scope :created_today, -> (created_at){ where("created_at >= ?", Date.current )}
+
+  def fullname_kanji
+    [family_name_kanji, first_name_kanji].join('')
+  end
+
 end
 
