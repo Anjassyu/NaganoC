2c64f39eb04eeee5f4dee05fb8ffae8b774d8893
vagrant <vagrant@localhost.localdomain>
2020-06-23 09:44:53 +0000

    [Add]注文機能にバリデーションを設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,9 @@ class Order < ApplicationRecord
 
 	enum payment: {クレジットカード:1, 銀行振込:2}
 
+	validates :customer_id, :address,:name, :postage,
+			  		:billing_amount, :payment,
+			  		presence: true
+	validates :postcode, length: {is: 7}, numericality: { only_integer: true }
+	validates :postage, :billing_amount, numericality: { only_integer: true }
 end
-
