e9d43d9fdc2f11292082223857cbf63ff92d745d
vagrant <vagrant@localhost.localdomain>
2020-06-23 09:56:54 +0000

    [Add]注文詳細にバリデーション追加

--- app/models/ordered_product.rb
+++ app/models/ordered_product.rb
@@ -1,6 +1,9 @@
 class OrderedProduct < ApplicationRecord
 	belongs_to :order
 	belongs_to :product
+	validates :product_id, :order_id, :count,
+			  		:price, presence: true
+	validates :price, :count, numericality: { only_integer: true }
 end
 
 
