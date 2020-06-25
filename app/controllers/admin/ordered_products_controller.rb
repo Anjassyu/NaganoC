257dc5278e371cad846d7a0289a0d41be2219a19
vagrant <vagrant@localhost.localdomain>
2020-06-25 11:57:11 +0000

    [Finish]デバッグ完了

--- app/controllers/admin/ordered_products_controller.rb
+++ app/controllers/admin/ordered_products_controller.rb
@@ -1,21 +1,27 @@
 class Admin::OrderedProductsController < ApplicationController
- before_action :authenticate_admin!
-def update
-	@ordered_product = OrderedProduct.find(params[:id])
-	if @ordered_product.update(ordered_product_params)
-	redirect_to admin_order_path(@ordered_product.order_id), notice: '情報を更新しました。'
-    else
-    	@ordered_products = OrderedProduct.all
-		@orders = Order.find(params[:id])
-    	render template: 'orders/show'
-    end
-end
 
-private
+	before_action :authenticate_admin!
+	
+	def update
+		@ordered_product = OrderedProduct.find(params[:id])
+		@ordered_product.update(ordered_product_params)
+		redirect_to admin_order_path(@ordered_product.order_id), notice: '情報を更新しました。'
 
-def ordered_product_params
-	params.require(:ordered_product).permit(:production_status)
+		@order = @ordered_product.order
+		if @ordered_product.production_status == 3
+			@order.update(order_status: "製作中")
+		end
 
-end
+		@ordered_products = OrderedProduct.where(order_id: @order)
+		if @ordered_products.where(production_status: 4).count == @ordered_products.where(order_id: @order).count
+			@order.update(order_status: "発送準備中")
+		end
+	end
 
+	private
+
+	def ordered_product_params
+		params.require(:ordered_product).permit(:production_status)
+
+	end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,15 +1,15 @@
 class Admin::OrdersController < ApplicationController
- 
- before_action :authenticate_admin!
+
+	before_action :authenticate_admin!
 	
 	def index
 		@orders = Order.all.page(params[:page]).per(10)
 		if params[:customer_id]
-	  	 @orders = Order.from_customer(params[:customer_id]).page(params[:page]).per(10)
+			@orders = Order.from_customer(params[:customer_id]).page(params[:page]).per(10)
 
 		elsif params[:created_at]
 
-		 @orders = Order.created_today(params[:creted_at]).page(params[:page]).per(10)
+			@orders = Order.created_today(params[:creted_at]).page(params[:page]).per(10)
 		end
 	end
 
@@ -21,31 +21,28 @@ class Admin::OrdersController < ApplicationController
 	def update
 		@order = Order.find(params[:id])
 		@order.update(order_params)
-		redirect_to admin_orders_path, notice: '情報を更新しました。'
+		@ordered_products = @order.ordered_products
+		redirect_to admin_order_path(@order), notice: '情報を更新しました。'
 		if @order.order_status == "入金確認"
-			@order.ordered_products.each do
-				@order.ordered_products.update(
-				production_status: 2)
-			end
+			@order.ordered_products.update(production_status: 2)
 		end
-		if 
 	end
 
 	private
 
 	def order_params
-	    params.require(:order).permit(:customer_id,
-	    							  :name,
-	    							  :postcode,
-	    							  :address,
-	    							  :postage,
-	    							  :billing_amount,
-	    							  :payment,
-	    							  :order_status)
+		params.require(:order).permit(:customer_id,
+			:name,
+			:postcode,
+			:address,
+			:postage,
+			:billing_amount,
+			:payment,
+			:order_status)
 	end
 	def ordered_product_params
-	params.require(:ordered_product).permit(:production_status)
+		params.require(:ordered_product).permit(:production_status)
 
-end
+	end
 end
 
