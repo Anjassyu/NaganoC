1fb5c3f94414407422318f609194a7857bcdf14b
vagrant <vagrant@localhost.localdomain>
2020-06-25 02:45:20 +0000

    first commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,29 +2,30 @@ class Order < ApplicationRecord
 	has_many :ordered_products, dependent: :destroy
 	belongs_to :customer
 
-  
+
 	validates :customer_id, :address,:name, :postage,
-			  		:billing_amount, :payment,
-			  		presence: true
+	:billing_amount, :payment,
+	presence: true
 	validates :postcode, length: {is: 7}, numericality: { only_integer: true }
 	validates :postage, :billing_amount, numericality: { only_integer: true }
 
 	enum payment: {クレジットカード:1, 銀行振込:2}
+	enum order_status: {入金待ち: 0,入金確認: 1,製作中: 2,発送準備中: 3, 発送済み: 4}
 
 
 	def fullname_kanji
-     [family_name_kanji, first_name_kanji].join('')
-    end
+		[family_name_kanji, first_name_kanji].join('')
+	end
 
-    def full_address
-     [postcode, address, fullname_kanji].join('')
-    end
+	def full_address
+		[postcode, address, fullname_kanji].join('')
+	end
 
 
-	 scope :from_customer, -> (customer_id){ where( customer_id: customer_id )}
-	 scope :created_today, -> (created_at){ where("created_at >= ?", Date.current )}
+	scope :from_customer, -> (customer_id){ where( customer_id: customer_id )}
+	scope :created_today, -> (created_at){ where("created_at >= ?", Date.current )}
 
- 
+
 end
 
 
