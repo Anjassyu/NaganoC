4c572e70a538752fd97df1926b9fa0219bc4c1fd
vagrant <vagrant@localhost.localdomain>
2020-06-25 08:55:08 +0000

    途中経過

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,7 @@
 class Admin::OrdersController < ApplicationController
+ 
  before_action :authenticate_admin!
+	
 	def index
 		@orders = Order.all.page(params[:page]).per(10)
 		if params[:customer_id]
@@ -12,18 +14,21 @@ class Admin::OrdersController < ApplicationController
 	end
 
 	def show
-		@ordered_products = OrderedProduct.all
 		@order = Order.find(params[:id])
+		@ordered_products = @order.ordered_products
 	end
 
 	def update
-		@ordered_products = OrderedProduct.all
 		@order = Order.find(params[:id])
-		if @order.update(order_params)
+		@order.update(order_params)
 		redirect_to admin_orders_path, notice: '情報を更新しました。'
-	    else
-	    	render "show"
-	    end
+		if @order.order_status == "入金確認"
+			@order.ordered_products.each do
+				@order.ordered_products.update(
+				production_status: 2)
+			end
+		end
+		if 
 	end
 
 	private
@@ -38,5 +43,9 @@ class Admin::OrdersController < ApplicationController
 	    							  :payment,
 	    							  :order_status)
 	end
+	def ordered_product_params
+	params.require(:ordered_product).permit(:production_status)
+
+end
 end
 
