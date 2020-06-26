a61a35b2cf03319e1d252c97b461c667cffd4e65
wakanak2 <w.koba.0214@gmail.com>
2020-06-26 18:02:03 +0900

    [ec商品一覧画面の修正]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(document).ready(function () {
+$(document).on('turbolinks:load',function () {
   $("#theTarget").skippr({
       transition : 'slide',
       speed : 1000,
